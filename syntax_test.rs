b21f123b3ab3b1e30d0a84aec84d04caf92790a2
XamadFP <76707171+XamadFP@users.noreply.github.com>
2021-04-11 15:10:31 -0400

    Testing w/ str literals instead of Strings

--- syntax_test.rs
+++ syntax_test.rs
@@ -1,13 +1,14 @@
-fn find_ace_high(arr: &[i32]) -> Option<(i32, [i32; 2], [(i32, String); 2])> {
+fn find_ace_high(arr: &[(i32, &str)]) -> Option<(i32, [i32; 2], [(i32, &'static str); 2])> {
 	match arr {
-		[ 1, .. ] => Some((1, [1, 0], [(1, String::from("H")), (3, String::from("C"))])),
-		[ _a, .. ] => Some((1, [2, 0], [(3, String::from("C")), (5, String::from("S"))])),
+		[ (1, "H"), .. ] => Some((1, [1, 0], [(1, "H"), (3, "C")])),
+		[ _a, .. ] => Some((1, [2, 0], [(3, "C"), (5, "S")])),
 		_ => None,
 	}
 }
 
 fn main() {
-	let x = find_ace_high(&[1, 2, 3]);
-	let x_unpacked = x.unwrap();
-	println!("{}{}", x_unpacked.2[0].0, x_unpacked.2[0].1);
+	let x = [(1, "C"), (2, "H")];
+	let y = find_ace_high(&x);
+	let y_unpacked = y.unwrap();
+	println!("{}{}", y_unpacked.2[0].0, y_unpacked.2[0].1);
 }
