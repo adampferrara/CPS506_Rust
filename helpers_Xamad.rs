ee5e47c00cebeaf8ac1aedac67ef73ae1efc465d
XamadFP <76707171+XamadFP@users.noreply.github.com>
2021-04-12 11:40:37 -0400

    Update helpers_Xamad.rs
    Changed formatting to be closer to what is outlined in the assignment pdf. Still needs rewriting to ensure it works properly.

--- helpers_Xamad.rs
+++ helpers_Xamad.rs
@@ -1,5 +1,23 @@
-fn find_ace_high(arr: &[(u32, &str)]) -> Option<(u32, [u32; 2], [(u32, &'static str); 2])> {
-	match arr {
+/*
+fn find_straight(vec: &[(u32, String)]) -> Option<(u32, [u32; 2], [(u32, &'static str); 2])> {
+	
+}
+
+fn find_three_of_kind(vec: &[(u32, String)]) -> Option<(u32, [u32; 2], [(u32, &'static str); 2])> {
+	
+}
+
+fn find_two_pair(vec: &[(u32, String)]) -> Option<(u32, [u32; 2], [(u32, &'static str); 2])> {
+	
+}
+
+fn find_pair(vec: &[(u32, String)]) -> Option<(u32, [u32; 2], [(u32, &'static str); 2])> {
+	
+}
+*/
+
+fn find_ace_high(vec: &[(u32, String)]) -> Option<(u32, [u32; 2], [(u32, String); 2])> {
+	match vec {
 		[ (1, s1), .. ] => Some((1, [1, 0], [(1, s1), (3, "C")])),
 		[ _a, .. ] => Some((1, [2, 0], [(3, "C"), (5, "S")])),
 		_ => None,
@@ -7,11 +25,11 @@ fn find_ace_high(arr: &[(u32, &str)]) -> Option<(u32, [u32; 2], [(u32, &'static 
 }
 
 /*
-fn deal(&[c1, c2, c3, c4, c5, c6, c7, c8, c9]: &[u32, u32, u32, u32, u32, u32, u32, u32, u32]) -> &[&str] {
-	let mut hand_a = [c1, c3, c5, c6, c7, c8, c9];
+pub fn deal(perm: &[u32;9]) -> Vec<String> {
+	let mut hand_a = [perm[0], perm[2], perm[4], perm[5], perm[6], perm[7], perm[8]];
 	hand_a.sort();
 	hand_a.reverse();
-	let mut hand_b = [c2, c4, c5, c6, c7, c8, c9];
+	let mut hand_b = [perm[1], perm[3], perm[4], perm[5], perm[6], perm[7], perm[8]];
 	hand_b.sort();
 	hand_b.reverse();
 	
