7691fc39442560f12333c1ca81a64e26d53d5784
XamadFP <76707171+XamadFP@users.noreply.github.com>
2021-04-13 13:04:01 -0400

    Working test of find_ace_high
    This is about the ugliest thing I've ever done, but this seems to work properly when it comes to getting it to run.
    I'm at the very least certain of the logic for matches working properly, it's just the syntax for sending and returning Vectors that's getting me tripped up it seems.
    match &* hand is necessary for it to recognize patterns in a Vector. There may be a better way of handling this.
    To ensure that a hand_a or hand_b Vector can be sent to multiple functions, it seems cloning is necessary.

--- syntax_test.rs
+++ syntax_test.rs
@@ -1,18 +1,33 @@
+fn find_ace_high(hand: Vec<(u32, String)>) -> Option<(u32, [u32; 2], Vec<(u32, String)>)> {
+	match &*hand {
+		[.., (x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)] if *x5 == 1 => 
+			Some((1, [*x5, 0], vec![(*x1, String::from(s1)), (*x2, String::from(s2)),
+				 (*x3, String::from(s3)), (*x4, String::from(s4)), (*x5, String::from(s5))])),
+		_ => None,
+	}
+}
+
 fn main() {
-	let x = (1, "C");
-	let mut hand_a = vec![x, (5, "D"), (5, "S"), (7, "C"), (8, "H"), (10, "C"), (13, "D")];
+	let x = (1, String::from("C"));
+	let mut hand_a =
+		vec![x, (5, String::from("D")), (5, String::from("S")), (7, String::from("C")),
+				(8, String::from("H")), (10, String::from("S")), (13, String::from("D"))];
 	//let mut hand_b = vec![x, (1, "S")];
 	hand_a.sort();
 	hand_a.reverse();
 	
+	/*
 	let y = match &*hand_a {
 		[.., (x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)] if x5 == &1 => 
 			Some((1, [x5, &0], vec![(x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)])),
 		_ => None,
 	};
+	*/
+	
+	let y = find_ace_high(hand_a.clone());
 	
 	println!("{:?}", &hand_a);
 	let y_unpacked = y.unwrap();
 	println!("{}, {}, Card 1: {}{}",
-			 y_unpacked.0, (y_unpacked.1 == [&1, &0]), y_unpacked.2[0].0, y_unpacked.2[0].1);
+			 y_unpacked.0, (y_unpacked.1 == [1, 0]), y_unpacked.2[0].0, y_unpacked.2[0].1);
 }
