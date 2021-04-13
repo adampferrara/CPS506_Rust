d3458411745aad153f07ed72d0ff1e85e00f1bb5
XamadFP <76707171+XamadFP@users.noreply.github.com>
2021-04-13 11:53:22 -0400

    Now w/ returning array with value from hand
    Highest value array [x5, &0] now returns a value from the hand instead of a raw integer. Can still be compared simply using [] >= [] if values are given as &int instead of int.

--- syntax_test.rs
+++ syntax_test.rs
@@ -7,12 +7,12 @@ fn main() {
 	
 	let y = match &*hand_a {
 		[.., (x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)] if x5 == &1 => 
-			Some((1, [1, 0], vec![(x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)])),
+			Some((1, [x5, &0], vec![(x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)])),
 		_ => None,
 	};
 	
 	println!("{:?}", &hand_a);
 	let y_unpacked = y.unwrap();
 	println!("{}, {}, Card 1: {}{}",
-			 y_unpacked.0, (y_unpacked.1 > [0, 0]), y_unpacked.2[0].0, y_unpacked.2[0].1);
+			 y_unpacked.0, (y_unpacked.1 == [&1, &0]), y_unpacked.2[0].0, y_unpacked.2[0].1);
 }
