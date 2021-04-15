b9b017972ee13086dda29972c862ba70fdf1283b
kalanG <73397353+kalan-g@users.noreply.github.com>
2021-04-15 01:57:41 -0400

    Update helpers_Gawsee.rs
    -I need double check 3,9,18 if '..,' in right places

--- helpers_Gawsee.rs
+++ helpers_Gawsee.rs
@@ -1,1 +1,24 @@
+fn find_royal_flush(hand: Vec<(u32, String)>) -> Option<(u32, [u32; 2], Vec<(u32, String)>)> {
+    match &*hand{
+        [..,(x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)]
+            if (*x1 == 13) && (*x2 == 12) && (*x3 == 11) && (*x4 == 10) && (*x5 == 1) &&
+            (s1 == s2) && (s1 == s3) && (s1 == s4) && (s1 == s5) =>
+            Some((10,[*x1,0], vec![(*x5, String::from(s5)), (*x4, String::from(s1)),
+                (*x3, String::from(s3)), (*x2, String::from(s2)), (*x1, String::from(s1))])),
+        
+        [..,(x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)]
+            if (*x1 == 13) && (*x2 == 12) && (*x3 == 11) && (*x4 == 10) && (*x5 == 1) &&
+            (s1 == s2) && (s1 == s3) && (s1 == s4) && (s1 == s5) =>
+            Some((10,[*x1,0], vec![(*x5, String::from(s5)), (*x4, String::from(s1)),
+                (*x3, String::from(s3)), (*x2, String::from(s2)), (*x1, String::from(s1))])),
+        _=> None,
 
+fn find_straight_flush(hand: Vec<(u32, String)>) -> Option<(u32, [u32; 2], Vec<(u32, String)>)> {
+    match &*hand{
+        [..,(x1, s1), (x2, s2), (x3, s3), (x4, s4), (x5, s5)]
+            if ((*x1 - *x2) == 1) && ((*x2 - *x3) == 1) && ((*x3 - *x4) == 1) && ((*x4 - *x5) == 1) && 
+            (s1 == s2) && (s1 == s3) && (s1 == s4) && (s1 == s5) =>
+            Some((9,[*x1,0], vec![(*x5, String::from(s5)), (*x4, String::from(s1)),
+                (*x3, String::from(s3)), (*x2, String::from(s2)), (*x1, String::from(s1))])),
+        _ => None,
+        
