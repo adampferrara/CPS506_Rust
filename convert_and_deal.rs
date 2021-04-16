4c810f44e1397aa2afced24e7ae94850212d8ed8
kalanG <73397353+kalan-g@users.noreply.github.com>
2021-04-15 22:31:50 -0400

    almsot done the best_hand
    -still need to add in the otherwise condition
    also im not 100% sure on the syntax, lemme know if i messed up somewhere

---
+++ convert_and_deal.rs
@@ -0,0 +1,36 @@
+fn best_hand(perm: Vec<(u32, String)>) -> Vec<(u32, [u32; 2], Vec<(u32, String)>)> {
+   
+        if (find_royal_flush(*hand_a).is_none()) = (find_royal_flush(*hand_a).unwrap())
+        if (find_royal_flush(*hand_b).is_none()) = (find_royal_flush(*hand_b).unwrap())
+        
+        if (find_straight_flush(*hand_a).is_none()) = (find_straight_flush(*hand_a).unwrap())
+        if (find_straight_flush(*hand_b).is_none()) = (find_straight_flush(*hand_b).unwrap())
+    
+        if (find_four_of_a_kind(*hand_a).is_none()) = (find_four_of_a_kind(*hand_a).unwrap())
+        if (find_four_of_a_kind(*hand_b).is_none()) = (find_four_of_a_kind(*hand_b).unwrap())
+    
+        if (find_full_house(*hand_a).is_none()) = (find_full_house(*hand_a).unwrap())
+        if (find_full_house(*hand_b).is_none()) = (find_full_house(*hand_b).unwrap())
+    
+        if (find_flush(*hand_a).is_none()) = (find_flush(*hand_a).unwrap())
+        if (find_flush(*hand_b).is_none()) = (find_flush(*hand_b).unwrap())
+    
+        if (find_straight(*hand_a).is_none()) = (find_straigt(*hand_a).unwrap())
+        if (find_straight(*hand_b).is_none()) = (find_straight(*hand_b).unwrap())
+    
+        if (find_three_of_a_kind(*hand_a).is_none()) = (find_three_of_a_kind(*hand_a).unwrap())
+        if (find_three_of_a_kind(*hand_b).is_none()) = (find_three_of_a_kind(*hand_b).unwrap())
+    
+        if (find_royal_flush(*hand_a).is_none()) = (find_royal_flush(*hand_a).unwrap())
+        if (find_royal_flush(*hand_b).is_none()) = (find_royal_flush(*hand_b).unwrap())
+    
+        if (find_royal_flush(*hand_a).is_none()) = (find_royal_flush(*hand_a).unwrap())
+        if (find_royal_flush(*hand_b).is_none()) = (find_royal_flush(*hand_b).unwrap())
+        
+        if (find_royal_flush(*hand_a).is_none()) = (find_royal_flush(*hand_a).unwrap())
+        if (find_royal_flush(*hand_b).is_none()) = (find_royal_flush(*hand_b).unwrap())
+        
+        
+   
+   
+   
