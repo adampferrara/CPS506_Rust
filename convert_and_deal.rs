270aed1a14b4c59db6d1972dff326c2e2b3878eb
XamadFP <76707171+XamadFP@users.noreply.github.com>
2021-04-15 23:02:45 -0400

    Update convert_and_deal.rs
    The method is called twice and only needs to handle one set at a time

--- convert_and_deal.rs
+++ convert_and_deal.rs
@@ -1,36 +1,23 @@
-fn best_hand(perm: Vec<(u32, String)>) -> Vec<(u32, [u32; 2], Vec<(u32, String)>)> {
+fn best_hand(hand: Vec<(u32, String)>) -> (u32, [u32; 2], Vec<(u32, String)>) {
    
-        if (find_royal_flush(*hand_a).is_none()) = (find_royal_flush(*hand_a).unwrap())
-        if (find_royal_flush(*hand_b).is_none()) = (find_royal_flush(*hand_b).unwrap())
+        if (find_royal_flush(*hand).is_none()) = (find_royal_flush(*hand).unwrap())
         
-        if (find_straight_flush(*hand_a).is_none()) = (find_straight_flush(*hand_a).unwrap())
-        if (find_straight_flush(*hand_b).is_none()) = (find_straight_flush(*hand_b).unwrap())
+        if (find_straight_flush(*hand).is_none()) = (find_straight_flush(*hand).unwrap())
     
-        if (find_four_of_a_kind(*hand_a).is_none()) = (find_four_of_a_kind(*hand_a).unwrap())
-        if (find_four_of_a_kind(*hand_b).is_none()) = (find_four_of_a_kind(*hand_b).unwrap())
+        if (find_four_of_kind(*hand).is_none()) = (find_four_of_kind(*hand).unwrap())
     
-        if (find_full_house(*hand_a).is_none()) = (find_full_house(*hand_a).unwrap())
-        if (find_full_house(*hand_b).is_none()) = (find_full_house(*hand_b).unwrap())
+        if (find_full_house(*hand).is_none()) = (find_full_house(*hand).unwrap())
     
-        if (find_flush(*hand_a).is_none()) = (find_flush(*hand_a).unwrap())
-        if (find_flush(*hand_b).is_none()) = (find_flush(*hand_b).unwrap())
+        if (find_flush(*hand).is_none()) = (find_flush(*hand).unwrap())
     
-        if (find_straight(*hand_a).is_none()) = (find_straigt(*hand_a).unwrap())
-        if (find_straight(*hand_b).is_none()) = (find_straight(*hand_b).unwrap())
+        if (find_straight(*hand).is_none()) = (find_straight(*hand).unwrap())
     
-        if (find_three_of_a_kind(*hand_a).is_none()) = (find_three_of_a_kind(*hand_a).unwrap())
-        if (find_three_of_a_kind(*hand_b).is_none()) = (find_three_of_a_kind(*hand_b).unwrap())
+        if (find_three_of_kind(*hand).is_none()) = (find_three_of_kind(*hand).unwrap())
     
-        if (find_royal_flush(*hand_a).is_none()) = (find_royal_flush(*hand_a).unwrap())
-        if (find_royal_flush(*hand_b).is_none()) = (find_royal_flush(*hand_b).unwrap())
+        if (find_two_pair(*hand).is_none()) = (find_two_pair(*hand).unwrap())
     
-        if (find_royal_flush(*hand_a).is_none()) = (find_royal_flush(*hand_a).unwrap())
-        if (find_royal_flush(*hand_b).is_none()) = (find_royal_flush(*hand_b).unwrap())
+        if (find_pair(*hand).is_none()) = (find_pair(*hand).unwrap())
         
-        if (find_royal_flush(*hand_a).is_none()) = (find_royal_flush(*hand_a).unwrap())
-        if (find_royal_flush(*hand_b).is_none()) = (find_royal_flush(*hand_b).unwrap())
+        if (find_ace_high(*hand).is_none()) = (find_ace_high(*hand).unwrap())
         
-        
-   
-   
-   
+}
